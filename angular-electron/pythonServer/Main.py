9fc0725330b3c28c3192da5672f7b8bc6bd50339
yoav-shaham <shaham.yoav2000@gmail.com>
2018-05-31 12:03:02 +0300

    DONE

--- angular-electron/pythonServer/Main.py
+++ angular-electron/pythonServer/Main.py
@@ -42,7 +42,7 @@ class api(object):
     commands=[
     {"commandName":"hey there"}
     ]
-    url='ec2-18-221-254-75.us-east-2.compute.amazonaws.com'
+    url='https://ec2-18-221-254-75.us-east-2.compute.amazonaws.com:3000'
     def initiating_listening(self):
         zerorpc.heartbeat.gevent.sleep(0)
         while True:
--- angular-electron/src/app/services/http-services/http.service.ts
+++ angular-electron/src/app/services/http-services/http.service.ts
@@ -7,13 +7,13 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getCommands(){
-    var x= this.http.get("ec2-18-221-254-75.us-east-2.compute.amazonaws.com:3000/command")
+    var x= this.http.get("http://ec2-18-221-254-75.us-east-2.compute.amazonaws.com:3000/command")
   
     console.debug(x)
     return x
   }
   postcommand(command){
-   var x = this.http.post("ec2-18-221-254-75.us-east-2.compute.amazonaws.com:3000/Command",command)
+   var x = this.http.post("http://ec2-18-221-254-75.us-east-2.compute.amazonaws.com:3000/Command",command)
   
     console.debug(x)
     return x
